b2dc576585cd2d7b3132e24b5f4306062a7c793a
Junaid Altaf <03-243201-004@student.bahria.edu.pk>
2022-02-22 10:40:01 +0500

    This is first comment

---
+++ four-number.py
@@ -0,0 +1,16 @@
+num1=int(input("enter first number \n "))
+num2=int(input("enter Second  number \n  "))
+num3=int(input("enter Third  number \n  "))
+num4=int(input("enter fourth  number  \n "))
+
+if(num1>num2 and num1>num3 and num1>num4):
+    print("Number one is greatest vaile ", num1)
+
+elif(num2>num1 and num2>num3 and num2>num4):
+    print("Number two is greatest vaile ", num2)
+
+elif(num3>num2 and num3>num1 and num3>num4):
+    print("Number three is greatest vaile ", num3)
+
+elif(num4>num2 and num4>num3 and num4>num1):
+    print("Number four  is greatest vaile ", num4)
